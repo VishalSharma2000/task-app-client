525b4d8b3a8dcfa62f57ba3e3817123a50ff859d
vishal <vishaljhsharma@gmail.com>
2021-01-20 20:54:32 +0530

    changed name of Login.css so that it can be used for sign up and login form, completed login form

--- src/components/forms/Login.js
+++ src/components/forms/Login.js
@@ -1,45 +1,67 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-import './Login.css';
+
+import './Form.css';
 
 const Login = () => {
   const [details, setDetails] = useState({
     email: "",
     password: ""
   });
+  const [error, setError] = useState('');
 
   const onChangeFormFields = (event) => {
     const { name, value } = event.target;
 
-    setDetails({...details, [name]: value});
+    setDetails({ ...details, [name]: value });
   };
 
   return (
     <div className="root">
       <h1 className="header">Login</h1>
+
       <form action="" method="POST" className="form-group">
-        <input 
+
+        {/* Showing error */}
+        {
+          error &&
+          <div className="form-error">
+            <span>{error}</span>
+            <span
+              className="close-btn"
+              onClick={() => setError(null)}
+            >
+              x</span>
+          </div>
+        }
+
+        {/* Form inputs */}
+        <input
           className="form-input"
-          type="email" 
-          placeholder="Email" 
+          type="email"
+          placeholder="Email"
           name="email"
           value={details.email}
           onChange={onChangeFormFields}
           autoFocus
+          required
         /> <br />
-        <input 
+        <input
           className="form-input"
-          type="password" 
-          placeholder="Password" 
+          type="password"
+          placeholder="Password"
           name="password"
           value={details.password}
           onChange={onChangeFormFields}
+          required
         /> <br />
-        <button 
+        <button
           className="form-button"
           type="submit"
-        >Login</button>
+        >SIGN IN</button>
       </form>
+
+      {/* Other Links */}
       <p className="form-text">Reset <Link to="#" className="form-link">Password</Link></p>
       <p className="form-text">Don't have account <Link to="/signup">Sign Up</Link></p>
     </div>
