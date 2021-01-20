1bd64b6a799ef4f3712ded93a6d3d04ad21bd3d4
vishal <vishaljhsharma@gmail.com>
2021-01-20 20:52:42 +0530

    Completed sign up form

--- src/components/forms/Signup.js
+++ src/components/forms/Signup.js
@@ -1,4 +1,14 @@
 import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+
+import "./Form.css";
+
+// check for all the needed validation before submitting the form
+const checkInputValidation = (details) => {
+  // check password and confirm password 
+  if (details.password !== details.confirmPassword) return "Passwords do not match";
+  if (details.password.length < 7) return "Password should be contain more than 6 characters";
+};
 
 const Signup = () => {
   const [details, setDetails] = useState({
@@ -7,34 +17,90 @@ const Signup = () => {
     password: "",
     confirmPassword: ""
   });
-  
+  const [error, setError] = useState('');
+
   const onChangeFormFields = (event) => {
     const { name, value } = event.target;
 
-    setDetails({ ...details, [name]: value });
-  }; 
+    setDetails({ ...details, [name]: value.trim() });
+  };
+
+  const onClickSubmit = (event) => {
+    event.preventDefault();
+
+    let errorVal = checkInputValidation(details);
+    if (errorVal) setError(errorVal);
+  };
 
   return (
-    <div>
-      <h1>Sign up</h1>
+    <div className="root">
+      <h1 className="header">Sign up</h1>
 
-      <form action="" method="POST">
-        <input 
+      <form action="" method="POST" onSubmit={onClickSubmit}>
+
+        {/* For showing error */}
+        {
+          error &&
+          <div className="form-error">
+            <span>{error}</span>
+            <span
+              className="close-btn"
+              onClick={() => setError(null)}
+            >
+              x</span>
+          </div>
+        }
+
+        {/* Form inputs */}
+        <input
+          className="form-input"
           type="text"
           placeholder="Full Name"
           name="name"
           value={details.name}
-          onChangeFormFields={onChangeFormFields}
-        />
+          onChange={onChangeFormFields}
+          required
+        /> <br />
 
-        <input 
-          type="text"
-          placeholder="Full Name"
-          name="name"
-          value={details.name}
-          onChangeFormFields={onChangeFormFields}
-        />
+        <input
+          className="form-input"
+          type="email"
+          placeholder="Email"
+          name="email"
+          value={details.email}
+          onChange={onChangeFormFields}
+          required
+        /> <br />
+
+        <input
+          className="form-input"
+          type="password"
+          placeholder="Password"
+          name="password"
+          value={details.password}
+          onChange={onChangeFormFields}
+          required
+        /> <br />
+
+        <input
+          className="form-input"
+          type="password"
+          placeholder="Confirm Password"
+          name="confirmPassword"
+          value={details.confirmPassword}
+          onChange={onChangeFormFields}
+          required
+        /> <br />
+
+        <button
+          className="form-button"
+        >
+          SIGN UP
+        </button>
       </form>
+
+      {/* Other Links */}
+      <span className="form-text">Already have an <Link to="/login">account?</Link></span>
     </div>
   );
 };
