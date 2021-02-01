16508c4271812c2defc810310232b8c7daa29eae
vishal <vishaljhsharma@gmail.com>
2021-02-01 23:04:40 +0530

    Added form for adding new taks and did some styling

--- src/components/User/Home.js
+++ src/components/User/Home.js
@@ -1,13 +1,18 @@
 import React, { useState, useEffect, useContext } from 'react';
 import { UserContext } from '../context/UserContext';
 
-import { fetchAllTasks } from '../../services/user/task';
+import Task from '../Task/Task';
+import { fetchAllTasks, addNewTask } from '../../services/user/task';
 import Navbar from '../Navbar/Navbar';
 import './Home.css';
 
 const Home = () => {
   const { currentUser, setCurrentUser } = useContext(UserContext);
   const [allTasks, setAllTasks] = useState([]);
+  const [newTask, setNewTask] = useState({
+    title: "",
+    description: "",
+  });
 
   useEffect(() => {
     const fetchTasks = async () => {
@@ -19,16 +24,43 @@ const Home = () => {
     fetchTasks();
   }, []);
 
-  console.log('All Tasks', allTasks);
+  const onClickAddTask = () => {
+
+  };
 
   return (
     <div>
       <Navbar />
 
       <div className="main-content">
-        <div
-          className="greet"
-        >{`Hello ${currentUser.user.name}`}</div>
+        {/* Rendering all tasks of the user */}
+        {allTasks.map((task, index) => (
+          <Task
+            task={task}
+            key={index}
+          />
+        ))}
+      </div>
+
+      {/* Add New Task */}
+      <div className="new-task">
+        <div className="new-task-inputs">
+          <input
+            type="text"
+            value={newTask.title}
+            placeholder="Title"
+            onChange={e => setNewTask({ ...newTask, title: e.target.value })}
+          />
+
+          <textarea
+            placeholder="Task Description"
+            onChange={e => setNewTask({ ...newTask, description: e.target.value })}
+          >{newTask.description}</textarea>
+        </div>
+
+        <button
+          onClick={onClickAddTask}
+        >Save</button>
       </div>
     </div>
   );
--- src/services/user/task.js
+++ src/services/user/task.js
@@ -10,4 +10,16 @@ export const fetchAllTasks = () => {
         reject(err.response);
       })
   });
-}; 
+};
+
+export const addNewTask = (task) => {
+  return new Promise((resolve, reject) => {
+    authAxios.post('/tasks/')
+      .then(response => {
+        console.log(response, 'Response after adding new task');
+      })
+      .catch(err => {
+        console.log('Some error', err);
+      })
+  });
+};
